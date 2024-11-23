b77716eb0169b16aa230c6f5222a60a673b8c547
bhargavram-fission <bhargavaram.gottam@fissionlabs.com>
2024-11-23 18:46:34 +0530

    Update vite.config.js
    for aws

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    host: '0.0.0.0', // Bind to all network interfaces
+    port: 5173,      // Optional: Specify the port
+  },
 })
